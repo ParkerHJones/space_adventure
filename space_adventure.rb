7276316898dc75c87e63c036e6381c6672c8b734
Parker Jones <parker.harold.jones@gmail.com>
2020-11-19 18:37:29 -0700

    initial

---
+++ space_adventure.rb
@@ -0,0 +1,14 @@
+#user can choose some startup options
+#goes on adventure
+#there's an outcome
+
+#using classes
+#use a gem 'faker'
+#pushing to git
+
+
+Class Beginning
+end
+
+Class Adventure
+end
